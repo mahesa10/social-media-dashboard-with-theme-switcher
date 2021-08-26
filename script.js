096e4c2f725f8da6c7e14cedf7b4c6fa2ff671c8
mahesa10 <44994692+mahesa10@users.noreply.github.com>
2021-08-26 09:15:15 +0700

    Add semantic main and fix style & script

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const white = 'white';
 const darkBackground = 'hsl(230, 17%, 14%)';
 const topBgLight = 'hsl(225, 100%, 98%)';
 const topBgDark = 'hsl(232, 19%, 15%)';
+const headerBorderLight = 'hsl(230, 22%, 74%)'
 const lightCard = 'hsl(227, 47%, 96%)';
 const darkCard = 'hsl(228, 28%, 20%)';
 const activeCardLight = 'hsl(232, 33%, 91%)';
@@ -15,6 +16,7 @@ function darkMode() {
     if (switcher.checked) {
         document.documentElement.style.setProperty('--bg-color', darkBackground);
         document.documentElement.style.setProperty('--top-bg', topBgDark);
+        document.documentElement.style.setProperty('--header-border', darkCard);
         document.documentElement.style.setProperty('--card-bg', darkCard);
         document.documentElement.style.setProperty('--card-bg-active', activeCardDark);
         document.documentElement.style.setProperty('--primary-text', white);
@@ -22,6 +24,7 @@ function darkMode() {
     } else {
         document.documentElement.style.setProperty('--bg-color', white);
         document.documentElement.style.setProperty('--top-bg', topBgLight);
+        document.documentElement.style.setProperty('--header-border', headerBorderLight);
         document.documentElement.style.setProperty('--card-bg', lightCard);
         document.documentElement.style.setProperty('--card-bg-active', activeCardLight);
         document.documentElement.style.setProperty('--primary-text', primaryTextLight);
@@ -29,4 +32,5 @@ function darkMode() {
     }    
 }
 
+window.addEventListener('load', () => switcher.checked = true);
 window.addEventListener('change', darkMode);
